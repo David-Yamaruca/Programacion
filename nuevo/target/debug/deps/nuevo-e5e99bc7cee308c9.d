c72af9ab96e42de4c9a87a8d58d124c549b23d2d
David-Yamaruca <david.yamaruca@gmail.com>
2026-06-15 17:14:20 -0400

    Añado proyecto de Rust: nuevo

---
+++ nuevo/target/debug/deps/nuevo-e5e99bc7cee308c9.d
@@ -0,0 +1,5 @@
+/data/data/com.termux/files/home/Programacion/nuevo/target/debug/deps/nuevo-e5e99bc7cee308c9.d: src/main.rs
+
+/data/data/com.termux/files/home/Programacion/nuevo/target/debug/deps/nuevo-e5e99bc7cee308c9: src/main.rs
+
+src/main.rs:
---
+++ nuevo/target/debug/nuevo.d
@@ -0,0 +1,1 @@
+/data/data/com.termux/files/home/Programacion/nuevo/target/debug/nuevo: /data/data/com.termux/files/home/Programacion/nuevo/src/main.rs
